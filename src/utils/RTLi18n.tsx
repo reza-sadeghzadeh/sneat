b5a1d604d625d783c097baccdc5599df00bf849b
Reza Sadeghzadeh <reza_sadeghzadeh01@yahoo.com>
2022-12-30 18:21:09 +0330

    fix: language set method

--- src/utils/RTLi18n.tsx
+++ src/utils/RTLi18n.tsx
@@ -3,16 +3,31 @@ import { I18nContext } from "../contexts"
 import { useRTL } from "../hooks"
 import { useTranslation } from "react-i18next"
 import { languageProfiles, Lngs } from "./language"
+import { useLocalStorage } from "@mantine/hooks"
 
 export const I18nProvider = ({ children }: { children: ReactNode }) => {
   const [lng, setLng] = useState(
     localStorage.getItem("i18nextLng") === Lngs.FA ? Lngs.FA : Lngs.EN
   )
+
   const { setRTL } = useRTL()
   const { i18n } = useTranslation()
 
   useEffect(() => {
-    i18n.changeLanguage(lng)
+    switch (lng) {
+      case Lngs.EN:
+        i18n.changeLanguage(Lngs.EN)
+        break
+
+      case Lngs.FA:
+        i18n.changeLanguage(Lngs.FA)
+        break
+
+      default:
+        i18n.changeLanguage(Lngs.FA)
+        break
+    }
+
     setRTL(languageProfiles.find((p) => p.locale === lng)?.dir === "rtl")
   }, [lng])
   return (
